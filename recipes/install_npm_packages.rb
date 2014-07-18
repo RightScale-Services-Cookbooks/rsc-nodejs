e7556daec01836d9aedabeff4d4a28a37afe6ae6
Curt Wilhelm <curt@rightscale.com>
2014-07-18 16:03:30 -0600

    only install packages if package.json exists

--- recipes/install_npm_packages.rb
+++ recipes/install_npm_packages.rb
@@ -6,8 +6,9 @@ include_recipe "nodejs::npm"
 
 bash "install npms from package" do
    code <<-EOH
-    #{node['nodejs']['dir']}/bin/npm install /var/www/nginx-default
+    #{node['nodejs']['dir']}/bin/npm install #{node[:app][:destination]}
   EOH
+  only_if do ::File.exists?("#{node[:app][:destomatopm]}/package.json") end
 end
 
 rightscale_marker :end
