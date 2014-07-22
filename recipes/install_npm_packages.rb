02655d0d49564314c7d56206c2c7e3d6b4c3b8b8
Curt Wilhelm <curt@rightscale.com>
2014-07-22 17:10:26 -0600

    fix attribute name

--- recipes/install_npm_packages.rb
+++ recipes/install_npm_packages.rb
@@ -8,7 +8,7 @@ bash "install npms from package" do
    code <<-EOH
     #{node['nodejs']['dir']}/bin/npm install #{node[:app][:destination]}
   EOH
-  only_if do ::File.exists?("#{node[:app][:destomatopm]}/package.json") end
+  only_if do ::File.exists?("#{node[:app][:destination]}/package.json") end
 end
 
 rightscale_marker :end
