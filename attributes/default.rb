950c4d7e8c75f58729eeb5cd9c984ce38a0bdc3a
Curt Wilhelm <curt@rightscale.com>
2014-07-22 16:40:59 -0600

    update destination

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,8 @@
 #  default[:app][:ip]="127.0.0.1"
 #end
 default[:app][:port]=80 #port is hard coded in nginx
-default[:app][:destination] = '/var/www/nginx-default'
+default[:app][:destination] = '/usr/share/nginx/html'
 default[:app][:user]='nginx'
 
 default[:nodejs][:version]="0.10.29"
+default[:nodejs][:app_name]="myapp"
--- recipes/do_update_code.rb
+++ recipes/do_update_code.rb
@@ -1,3 +1,4 @@
+rightscale_marker :begin
 
 node.override[:app][:destination] = node[:repo][:default][:destination]
 
@@ -9,9 +10,12 @@ log "  Downloading project repo"
 # Calling "repo" LWRP to download remote project repository
 # See cookbooks/repo/resources/default.rb for the "repo" resource.
 repo "default" do
-  destination node[:app][:destination]
+  destination "#{node[:app][:destination]}"
   action node[:repo][:default][:perform_action].to_sym
   app_user node[:app][:user]
   repository node[:repo][:default][:repository]
   persist false
 end
+
+
+rightscale_marker :end
