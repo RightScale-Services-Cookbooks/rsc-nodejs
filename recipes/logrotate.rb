f794038319a71415d7abb9a8c777f2479cce1851
Curt Wilhelm <curt@rightscale.com>
2014-08-28 17:08:40 -0600

    correct log path for logrotate

--- recipes/logrotate.rb
+++ recipes/logrotate.rb
@@ -2,7 +2,7 @@ rightscale_marker :begin
 log "Setup Logrotate for nodejs"
 logrotate_app "nodejs npm" do
   cookbook "logrotate"
-  path "/var/log/nginx/nodejs.log"
+  path "/var/log/nginx/*.log"
   frequency "weekly"
   rotate 30
   create "644 root adm"
